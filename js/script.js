20438ee06d4c0dbcb3edf22110812d197488c4d1
ValeraKarimov <valeraguess@gmail.com>
2024-05-24 21:35:19 +0200

    chess letters like figures

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+let map = [];
+function initMap () {
+    // map [x] [y]
+    map = [
+    //   y0   y1   y2
+        ["R", "P", " ", " ", " ", " ", "p", "r",], // x = 0
+        ["N", "P", " ", " ", " ", " ", "p", "n",], // x = 1
+        ["B", "P", " ", " ", " ", " ", "p", "b",], // x = 2
+        ["Q", "P", " ", " ", " ", " ", "p", "q",], // x = 3
+        ["K", "P", " ", " ", " ", " ", "p", "k",], // x = 4
+        ["B", "P", " ", " ", " ", " ", "p", "b",], // x = 5
+        ["N", "P", " ", " ", " ", " ", "p", "n",], // x = 6
+        ["R", "P", " ", " ", " ", " ", "p", "r",]  // x = 7
+    ]
+}
 function showMap () {
     html = "<table border='1' cellpadding='2' cellspacing='0'>";
     for (let y = 7; y >= 0; y--)
@@ -7,7 +22,9 @@ function showMap () {
         for (let x = 0; x <= 7; x++)
             {
                 color = (x + y) % 2 ? '#eeffee' : '#abcdef';
-                html += "<td height=50 width=50 " + "style='background-color: "+color+"'></td>";
+                html += "<td height=50 width=50 " + "style='background-color: "+color+"; text-align: center'>";
+                html += map [x] [y]
+                html +="</td>";
             }
             html += "</tr>";
     }
@@ -18,4 +35,5 @@ function showMap () {
 
     document.getElementById ("board").innerHTML = html;
 }
+initMap();
 showMap();
