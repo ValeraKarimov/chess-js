71f8623f65ce0b222e33a78c6714dd73fa5b4800
ValeraKarimov <valeraguess@gmail.com>
2024-05-24 22:53:13 +0200

    figures one desk

--- js/script.js
+++ js/script.js
@@ -13,6 +13,17 @@ function initMap () {
         ["R", "P", " ", " ", " ", " ", "p", "r",]  // x = 7
     ]
 }
+function figureToHTML (figure) {
+    switch (figure) {
+        case "K" : return "&#9812;"; case "k" : return "&#9818;";
+        case "Q" : return "&#9813;"; case "q" : return "&#9819;";
+        case "R" : return "&#9814;"; case "r" : return "&#9820;";
+        case "B" : return "&#9815;"; case "b" : return "&#9821;";
+        case "N" : return "&#9816;"; case "n" : return "&#9822;";
+        case "P" : return "&#9817;"; case "p" : return "&#9823;";
+        default : return "&nbsp;";
+    }
+}
 function showMap () {
     html = "<table border='1' cellpadding='2' cellspacing='0'>";
     for (let y = 7; y >= 0; y--)
@@ -22,8 +33,12 @@ function showMap () {
         for (let x = 0; x <= 7; x++)
             {
                 color = (x + y) % 2 ? '#eeffee' : '#abcdef';
-                html += "<td height=50 width=50 " + "style='background-color: "+color+"; text-align: center'>";
-                html += map [x] [y]
+                html += "<td height=50 width=50 " + 
+                        "style='background-color: "+color+";" + 
+                        "text-align: center;" + 
+                        "font-size: 40px; " +
+                        "color: #000000'>";
+                html += figureToHTML (map [x] [y]);
                 html +="</td>";
             }
             html += "</tr>";
